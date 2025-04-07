b7359c99f509064150690721b845f72bace90553
unknown <amalfathieh@gmail.com>
2025-04-07 13:00:40 -0700

    some edit

--- app/services/CarService.php
+++ app/services/CarService.php
@@ -7,7 +7,6 @@ namespace App\services;
 
 use App\Http\Responses\Response;
 use App\Models\Car;
-use http\Env\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CarService
@@ -18,24 +17,6 @@ class CarService
     {
         $this->fileService = $fileService;
     }
-
-//    public function create($request){
-//
-//        $data = $request->except('images');
-//        $data['user_id'] = Auth::user()->id;
-//
-//        if ($request->hasFile('images')) {
-//            foreach ($request->file('images') as $image) {
-//                $name = $this->fileService->upload($image, "carsImages");
-//                $images[] = $name;
-//            }
-//            $data['images'] =json_encode($images);
-//        }
-//
-//        $car = Car::create($data);
-//
-//        return $car;
-//    }
 
     public function create($request)
     {
@@ -53,21 +34,8 @@ class CarService
         $car = Car::create($data);
         return $car;
     }
-//    public function update( $request, $car){
-//        $data = $request->except('images');
-//        $images = json_decode($car->images);
-//
-//        if ($request->hasFile('images')) {
-//            foreach ($request->file('images') as $image) {
-//                $name = $this->fileService->upload($image, "carsImages");
-//                $images[] = $name;
-//            }
-//            $data['images'] =json_encode($images);
-//        }
-//
-//        $car->update($data);
-//        return $car;
-//    }
+
+
     public function update($request, $car)
     {
         $data = $request->except('images');
@@ -85,17 +53,6 @@ class CarService
         return $car;
     }
 
-//    public function delete($car){
-//        if($car->user_id != Auth::user()->id){
-//            return Response::Error('unauthorized');
-//        }
-//
-//        $car->delete();
-//        $images = json_decode($car->images);
-//        foreach ($images as $image){
-//            $this->fileService->delete($image);
-//        }
-//    }
 
     public function delete($car)
     {
